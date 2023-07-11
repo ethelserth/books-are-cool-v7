560609acf6bd951f6e4a844914faa4a8419a5f5c
ethelserth <pergaminh@live.com>
2023-07-11 08:03:22 +0300

    change font

--- pages/_document.js
+++ pages/_document.js
@@ -18,45 +18,8 @@ class MyDocument extends Document {
     return (
       <Html lang={config.lang} className={cn(initialColorScheme)}>
         <Head>
-          {config.font && config.font === 'serif'
-            ? (
-            <>
-              <link
-                rel="preload"
-                href="/fonts/SourceSerif.var.woff2"
-                as="font"
-                type="font/woff2"
-                crossOrigin="anonymous"
-              />
-              <link
-                rel="preload"
-                href="/fonts/SourceSerif-Italic.var.woff2"
-                as="font"
-                type="font/woff2"
-                crossOrigin="anonymous"
-              />
-            </>
-              )
-            : (
-            <>
-              <link
-                rel="preload"
-                href="/fonts/IBMPlexSansVar-Roman.woff2"
-                as="font"
-                type="font/woff2"
-                crossOrigin="anonymous"
-              />
-              <link
-                rel="preload"
-                href="/fonts/IBMPlexSansVar-Italic.woff2"
-                as="font"
-                type="font/woff2"
-                crossOrigin="anonymous"
-              />
-            </>
-              )}
 
-          {['zh', 'ja', 'ko'].includes(
+          {['zh', 'ja', 'ko','el'].includes(
             config.lang.slice(0, 2).toLocaleLowerCase()
           ) && (
             <>
@@ -68,20 +31,20 @@ class MyDocument extends Document {
               <link
                 rel="preload"
                 as="style"
-                href={`https://fonts.googleapis.com/css2?family=Noto+${
+                href={`https://fonts.googleapis.com/css2?family=Roboto+${
                   config.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <link
                 rel="stylesheet"
-                href={`https://fonts.googleapis.com/css2?family=Noto+${
+                href={`https://fonts.googleapis.com/css2?family=Roboto+${
                   config.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <noscript>
                 <link
                   rel="stylesheet"
-                  href={`https://fonts.googleapis.com/css2?family=Noto+${
+                  href={`https://fonts.googleapis.com/css2?family=Roboto+${
                     config.font === 'serif' ? 'Serif' : 'Sans'
                   }+${CJK()}:wght@400;500;700&display=swap`}
                 />
