8de2f0748b65bd4be00e1b187a18e054ed3b18f9
ethelserth <pergaminh@live.com>
2023-07-15 19:51:31 +0300

    test2 + css

---
+++ pages/blog.config.js
@@ -0,0 +1,62 @@
+const BLOG = {
+  title: 'Books Are Cool',
+  author: '',
+  email: 'write@booksarecool.gr',
+  link: 'https://www.booksarecool.gr',
+  description: 'Μικρές κριτικές για μεγάλα βιβλία',
+  lang: 'el', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  timezone: 'Europe/Athens', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  appearance: 'dark', // ['light', 'dark', 'auto'],
+  font: 'serif', // ['sans-serif', 'serif']
+  lightBackground: '##fffbf0', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#18181B', // use hex value, don't forget '#'
+  path: '', // leave this empty unless you want to deploy Nobelium in a folder
+  since: 2023, // If leave this empty, current year will be used.
+  postsPerPage: 10,
+  sortByDate: true,
+  showAbout: false,
+  showArchive: true,
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://twitter.com/craigaryhart',
+  seo: {
+    keywords: ['λογοτεχνία', 'literature', 'books'],
+    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+  },
+  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+  analytics: {
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    ackeeConfig: {
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+    },
+    gaConfig: {
+      measurementId: 'G-8EGRDLSF2B' // e.g: G-XXXXXXXXXX
+    }
+  },
+  comment: {
+    // support provider: gitalk, utterances, cusdis
+    provider: '', // leave it empty if you don't need any comment plugin
+    gitalkConfig: {
+      repo: '', // The repository of store comments
+      owner: '',
+      admin: [],
+      clientID: '',
+      clientSecret: '',
+      distractionFreeMode: false
+    },
+    utterancesConfig: {
+      repo: ''
+    },
+    cusdisConfig: {
+      appId: '', // data-app-id
+      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+    }
+  },
+  isProd: 'development' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+// export default BLOG
+module.exports = BLOG
