b25280b1a14a71e8afed87aa7effec710a831251
ethelserth <pergaminh@live.com>
2023-07-16 23:07:16 +0300

    resolve?

--- pages/feed.js
+++ pages/feed.js
@@ -1,6 +1,13 @@
 import { getAllPosts } from '@/lib/notion'
 import { generateRss } from '@/lib/rss'
+
+import path from 'path'
+import fs from 'fs'
+
 export async function getServerSideProps ({ res }) {
+
+  const BGP2 = fs.readFileSync(path.join(process.cwd(), 'blog.config.js'), 'utf-8')
+
   res.setHeader('Content-Type', 'text/xml')
   const posts = await getAllPosts({ includePages: false })
   const latestPosts = posts.slice(0, 10)
