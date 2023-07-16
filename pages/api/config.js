9d1d1eff580d9ea87e9d316dedf298e7fc2cff68
ethelserth <pergaminh@live.com>
2023-07-16 21:49:34 +0300

    test3

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,8 +1,8 @@
-import { clientConfig } from '@/lib/server/config'
+import { config } from '@/lib/server/config'
 
 export default function handler (req, res) {
   if (req.method === 'GET') {
-    res.status(200).json(clientConfig)
+    res.status(200).json(config)
   } else {
     res.status(204).end()
   }
