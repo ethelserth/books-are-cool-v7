bb93d5fcb40361673165673313a1a79faeec127a
ethelserth <pergaminh@live.com>
2023-07-17 21:17:42 +0300

    test

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,10 +1,5 @@
 import { clientConfig } from '@/lib/server/config'
 
-import path from 'path'
-import fs from 'fs'
-
-const BGP5 = fs.readFileSync(path.join(process.cwd(), 'blog.config.js'), 'utf-8')
-
 export default function handler (req, res) {
   if (req.method === 'GET') {
     res.status(200).json(clientConfig)
