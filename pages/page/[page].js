23b23a94d58ff7fd0515ead4e000f54616b0319c
ethelserth <pergaminh@live.com>
2023-08-28 20:58:13 +0300

    Update [page].js

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -21,7 +21,7 @@ const Page = ({ postsToShow, page, showNext }) => {
 export async function getStaticProps (context) {
   const BGP4 = fs.readFileSync(path.join(process.cwd(), 'blog.config.js'), 'utf-8')
 
-  const { page } = context.params // Get Current Page No.
+  const { page } = context.params // Get Current Page No. s
   const posts = await getAllPosts({ includePages: false })
   const postsToShow = posts.slice(
     config.postsPerPage * (page - 1),
