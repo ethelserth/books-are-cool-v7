634111ccad2c535ac65608db69949179025ef66f
ethelserth <pergaminh@live.com>
2023-09-18 21:40:15 +0300

    Update Vercel.js

--- components/Vercel.js
+++ components/Vercel.js
@@ -11,7 +11,7 @@ const Vercel = () => {
         height="28"
         viewBox="0 0 135 28"
         fill="none"
-        xmlns="http://www.w3.org/2000/svg" //test
+        xmlns="http://www.w3.org/2000/svg" //test sda
         className="inline-block"
       >
         <g clipPath="url(#clip0)">
